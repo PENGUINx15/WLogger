bb52f20ad4b1c5d587df87870eb22b17560e3601
PENGUINx13 <103443268+PENGUINx15@users.noreply.github.com>
2026-02-22 18:39:40 +0100

    Fix startup issues and command registration

--- src/main/java/me/penguinx13/wLogger/WLogger.java
+++ src/main/java/me/penguinx13/wLogger/WLogger.java
@@ -5,19 +5,18 @@ import me.penguinx13.wapi.Managers.SQLiteManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.Objects;
 
 public final class WLogger extends JavaPlugin {
     private ConfigManager configManager;
     private SQLiteManager sqliteManager;
+
     @Override
     public void onEnable() {
         configManager = new ConfigManager(this);
         configManager.registerConfig("config.yml");
-        sqliteManager = new SQLiteManager(this, "players.db");
 
+        sqliteManager = new SQLiteManager(this, "players.db");
         sqliteManager.connect();
 
         sqliteManager.executeUpdate(
@@ -25,30 +24,26 @@ public final class WLogger extends JavaPlugin {
                         "playerName TEXT PRIMARY KEY," +
                         "backpack INTEGER NOT NULL DEFAULT 50," +
                         "costmultiplier DOUBLE NOT NULL DEFAULT 1.0," +
-                        "brokenBlocks INTEGER NOT NULL DEFAULT 0," +
+                        "brokenBlocks INTEGER NOT NULL DEFAULT 0" +
                         ")"
-        );
-
-        sqliteManager.executeUpdate(
-                "INSERT INTO players (uuid, coins) VALUES (?, ?) " +
-                        "ON CONFLICT(uuid) DO UPDATE SET coins = excluded.coins",
-                "player-uuid",
-                150
         );
 
         BlockBreakListener blockBreakListener = new BlockBreakListener(configManager, this);
         getServer().getPluginManager().registerEvents(blockBreakListener, this);
 
-        Objects.requireNonNull(getCommand("wlogger")).setExecutor(new CommandsExecutor(this, configManager));
+        Objects.requireNonNull(getCommand("wlogger"), "Command wlogger is not defined in plugin.yml")
+                .setExecutor(new CommandsExecutor(this, configManager));
 
         if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
-            new Placeholders(this).register();
-            getLogger().info("Плейсхолдеры WMine зареестрированы успешно!");
+            new Placeholders().register();
+            getLogger().info("Плейсхолдеры WLogger зарегистрированы успешно!");
         }
     }
 
     @Override
     public void onDisable() {
-
+        if (sqliteManager != null) {
+            sqliteManager.disconnect();
+        }
     }
 }
