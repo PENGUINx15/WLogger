f5dae26c3d2572d3bc00a71e685465ec6452f23b
PENGUINx13 <demchuk250706@gmail.com>
2026-03-03 16:15:33 +0100

    03.03.2026

--- src/main/java/me/penguinx13/wLogger/BlockBreakListener.java
+++
@@ -1,7 +0,0 @@
-package me.penguinx13.wLogger;
-
-@Deprecated
-public final class BlockBreakListener {
-    private BlockBreakListener() {
-    }
-}
--- src/main/java/me/penguinx13/wLogger/commands/Commands.java
+++
@@ -1,7 +0,0 @@
-package me.penguinx13.wLogger.commands;
-
-@Deprecated
-public final class Commands {
-    private Commands() {
-    }
-}
